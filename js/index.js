a4ef539632efd38faa593fd32eea55559c1bdd82
tytyu073iop <fpm.biryuk@bsu.by>
2025-03-04 13:16:10 +0300

    final

---
+++ js/index.js
@@ -0,0 +1,48 @@
+let hash = {}; //hey - plant, value - how to take care
+
+function Add() {
+    AddValue(prompt("plant: "), prompt("how to take care: "));
+}
+
+function DeleteB() {
+    DeleteValue(prompt("plant: "));
+}
+
+function GetValue() {
+    alert(GetValueInfo(prompt("plant: ")));
+}
+
+function allval() {
+    alert(ListValues());
+}
+
+function AddValue(key, value) {
+    if (key in hash) {
+        alert("Value is already here: " + key);
+        return;
+    }
+
+    console.log(key, value);
+    hash[key] = value;
+    return;
+}
+
+function DeleteValue(key) {
+    delete hash[key];
+}
+
+function GetValueInfo(key) {
+    if (key in hash) {
+        return "key: " + key + ", value: " + hash[key]; 
+    } else {
+        return "No info";
+    }
+}
+
+function ListValues() {
+    let string = "";
+    for (let key in hash) {
+        string += "key: " + key + ", value: " + hash[key] + "\n";
+    }
+    return string;
+}
